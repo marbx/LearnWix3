1be296e9da3385e0b084149ba326b7d187493c27
Markus <markuskramerigitt@gmail.com>
2021-07-22 21:51:24 +0200

    ReadConfig_IMCAC logic

--- exercise/CustomAction01/CustomAction01.cs
+++ exercise/CustomAction01/CustomAction01.cs
@@ -37,28 +37,36 @@ namespace CustomAction01 {
 
         [CustomAction]
         public static ActionResult ReadConfig_IMCAC(Session session) {
-            // IMMEDIATE means
-            //   you should not modify the system because the install transaction has not yet started
-            //   reading is fine
-            // Perform consistency checks on the configuration (old and new mixtures)
+            // Immediate means:
+            //  - you should not modify the system because the install transaction has not yet started
+            //  - reading is fine
+            // Perform consistency checks on existing configuration: WriteConfig_DECAC must not fail.
             session.Log("...Begin ReadConfig_IMCAC");
             string Manufacturer  = get_property_IMCAC(session, "Manufacturer");
             string ProductName   = get_property_IMCAC(session, "ProductName");
-            string MOVE_CONF     = get_property_IMCAC(session, "MOVE_CONF");
+            string MINION_CONFIG = get_property_IMCAC(session, "MINION_CONFIG");
             string ProgramData   = System.Environment.GetEnvironmentVariable("ProgramData");
             string CONFIGDIR_OLD = @"C:\" + ProductName;
             string CONFIGDIR_NEW =  ProgramData + @"\" + Manufacturer + @"\" + ProductName;
             session["CONFIGDIR_OLD"] = CONFIGDIR_OLD;
             session["CONFIGDIR_NEW"] = CONFIGDIR_NEW;
 
-            string abortReason = "";
-            if (MOVE_CONF == "1") {
+            string AbortReason = "";
+            // Find reasons to abort
+            if (MINION_CONFIG.Length == 0) {
+                // Care about existing configuration.
+                // Consistency check
                 if (Directory.Exists(CONFIGDIR_OLD) && Directory.Exists(CONFIGDIR_NEW)) {
-                    abortReason = CONFIGDIR_OLD + " and " + CONFIGDIR_NEW + " must not both exist when MOVE_CONF=1.  ";
+                    AbortReason = CONFIGDIR_OLD + " and " + CONFIGDIR_NEW + " must not both exist.";
                 }
+            } else {
+                session.Log("...MINION_CONFIG will replace any existing configuration.");
             }
-            if (abortReason.Length > 0) {
-                session["AbortReason"] = abortReason;
+            if (AbortReason.Length == 0) {
+            // Found no reasons to abort, try to read master and id.
+            } else {
+                // Abort install
+                session["AbortReason"] = AbortReason;
             }
             session.Log("...End ReadConfig_IMCAC");
             return ActionResult.Success;
@@ -67,9 +75,8 @@ namespace CustomAction01 {
 
         [CustomAction]
         public static ActionResult WriteConfig_DECAC(Session session) {
-            // DEFERRED means
-            //   you may modify the system because the install transaction has started
-            // If key does not exist, the msi will fail to install
+            // Deferred means:
+            //  - you may modify the system because the install transaction has started
             session.Log("...Begin WriteConfig_DECAC");
             string CONFIGDIR_OLD = get_property_DECAC(session, "CONFIGDIR_OLD");
             string CONFIGDIR_NEW = get_property_DECAC(session, "CONFIGDIR_NEW");
